c7f3fc594567bb25155fa04e7311b15f6982710c
Diep Nguyen <nguyenthanhdiep21051995@gmail.com>
2020-01-28 10:38:52 +0700

    router-vue features

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,32 @@
         <ul>
           <li class="nav-item">
             <router-link class="nav-link" :to="{name: 'Home'}" exact>
-              <img src="./assets/build-a-bot-logo.png" alt="Build a bot logo img" class="logo">
+              <img src="./assets/build-a-bot-logo.png" alt="Build a bot logo img" class="logo" />
               Build-a-Bot
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Build'}" exact>
-              Build
-            </router-link>
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>Build</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link"
+            :to="{name: 'BrowseParts'}" exact>Browse Parts</router-link>
           </li>
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view />
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar" />
+      </aside>
+      <main>
+        <router-view /> <!-- name="default" -->
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app',
 };
@@ -36,18 +42,17 @@ body {
   background-attachment: fixed;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -70,5 +75,16 @@ ul {
 }
 .router-link-active {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
